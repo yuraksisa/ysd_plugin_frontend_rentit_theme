2596287ae813fe2a1eefab607b88704dd3206799
Juan Gil Miqueo <jgil@MacBook-Pro-de-Juan.local>
2018-01-23 14:33:37 +0100

    footer info

--- lib/ysd_plugin_frontend_rentit_theme/yito_integration/ysd_plugin_frontend_rentit_theme_extension.rb
+++ lib/ysd_plugin_frontend_rentit_theme/yito_integration/ysd_plugin_frontend_rentit_theme_extension.rb
@@ -43,7 +43,24 @@ module YsdPluginFrontendRentitTheme
       # Apply the layout
       if layout_name == 'page_render' and SystemConfiguration::Variable.get_value('frontend.skin', nil) == 'rentit'
         # theme attributes
-        theme_attributes = {'css_header_background' => SystemConfiguration::Variable.get_value('frontend.skin.rentit.css.header_background','#fbfbfb') }
+        theme_attributes = {'css_header_background' => SystemConfiguration::Variable.get_value('frontend.skin.rentit.css.header_background','#fbfbfb'), 
+                            'site_company_name' => SystemConfiguration::Variable.get_value('site.company.name', '.'),
+                            'site_company_document_id' => SystemConfiguration::Variable.get_value('site.company.document_id', '.'),
+                            'site_company_phone_number' => SystemConfiguration::Variable.get_value('site.company.phone_number', '.'),
+                            'site_company_email' => SystemConfiguration::Variable.get_value('site.company.email', '.'),
+                            'site_company_address_1' => SystemConfiguration::Variable.get_value('site.company.address_1', '.'),
+                            'site_company_address_2' => SystemConfiguration::Variable.get_value('site.company.address_2', '.'),
+                            'site_company_city' => SystemConfiguration::Variable.get_value('site.company.city', '.'),
+                            'site_company_state' => SystemConfiguration::Variable.get_value('site.company.state', '.'),
+                            'site_company_country' => SystemConfiguration::Variable.get_value('site.company.country', '.'),
+                            'site_company_zip' => SystemConfiguration::Variable.get_value('site.company.zip', '.'),
+                            'site_company_facebook' => SystemConfiguration::Variable.get_value('site.company.facebook', '.'),
+                            'site_company_twitter' => SystemConfiguration::Variable.get_value('site.company.twitter', '.'),
+                            'site_company_linkedin' => SystemConfiguration::Variable.get_value('site.company.linkedin', '.'),
+                            'site_company_instagram' => SystemConfiguration::Variable.get_value('site.company.instagram', '.'),
+                            'year' => Date.today.year
+                           }
+        
         # template
         template_file = File.open (File.expand_path(File.join(File.dirname(__FILE__), '..', '..', '..', 'views','rentit_page_render.erb')))
         template = template_file.read
